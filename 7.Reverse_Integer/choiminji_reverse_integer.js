74a7e322af4733f8c3b2f582585f8fb09e77dda2
mj <eunoia2040@gmail.com>
2021-08-14 10:38:00 +0900

    edit logic

--- 7.Reverse_Integer/choiminji_reverse_integer.js
+++ 7.Reverse_Integer/choiminji_reverse_integer.js
@@ -1,9 +1,6 @@
 let reverse = function(x) {  
     let reverseNumber = String(Math.abs(x)).split('').reverse().join('');
-    
-    if (reverseNumber.indexOf(0) === 0 && reverseNumber.length != 1) {
-        Number(reverseNumber.substring(1));
-    }
+    reverseNumber = Number(reverseNumber);
       
     if (0x7FFFFFFF < reverseNumber) {
         return 0;

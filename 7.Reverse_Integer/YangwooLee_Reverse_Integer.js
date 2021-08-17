13bae9a9fb74bb969450df9934fdba208bf8d6dc
Kimin, Kim <83983303+kimin3927@users.noreply.github.com>
2021-08-17 14:24:14 +0900

    Update YangwooLee_Reverse_Integer.js

--- 7.Reverse_Integer/YangwooLee_Reverse_Integer.js
+++ 7.Reverse_Integer/YangwooLee_Reverse_Integer.js
@@ -1,6 +1,6 @@
 var reverse = function(x) {
     if(x === 0) return 0;
-    let result = parseInt(x.toString().split("").reverse().join(""));
+    let result = parseInt(x.toString().split("").reverse().join(""));//기민 
     if(result > Math.pow(2,31)-1 || result < -Math.pow(2,31)-1) return 0;
     return x > 0 ? result : -result
 };

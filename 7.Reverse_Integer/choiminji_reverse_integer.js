2862b20d2adf84c0d3e403408762dbb82125cf8c
MINJI <54696956+CoRoRo10@users.noreply.github.com>
2021-08-17 14:49:24 +0900

    Update choiminji_reverse_integer.js

--- 7.Reverse_Integer/choiminji_reverse_integer.js
+++ 7.Reverse_Integer/choiminji_reverse_integer.js
@@ -1,5 +1,9 @@
 let reverse = function(x) {  
     let reverseNumber = Number(String(Math.abs(x)).split('').reverse().join(''));
     
+    if (0x7FFFFFFF < reverseNumber) {
+        return 0;
+    }
+    
     return x < 0 ? -reverseNumber : reverseNumber;
 };

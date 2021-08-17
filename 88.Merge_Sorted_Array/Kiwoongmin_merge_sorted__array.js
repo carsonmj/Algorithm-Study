9af4dd2384bdc01d0bc91fc3a6a43be552df6894
dndals <86826505+dndals@users.noreply.github.com>
2021-08-17 13:51:54 +0900

    Update Kiwoongmin_merge_sorted__array.js

--- 88.Merge_Sorted_Array/Kiwoongmin_merge_sorted__array.js
+++ 88.Merge_Sorted_Array/Kiwoongmin_merge_sorted__array.js
@@ -1,1 +1,25 @@
-
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    const length = m + n;
+    let temp = '';
+    
+    for(let i = m; i < length ; i++) {
+        nums1[i] = nums2[i - m];
+    }
+  
+    for(let j = 0; j < length; j++) {
+        for(let k = j + 1; k < length; k++) {
+            if (nums1[j] > nums1[k]) {
+                temp = nums1[j];
+                nums1[j] = nums1[k];
+                nums1[k] = temp;
+            } 
+        }
+    }
+};

e3f76ccfcc25532390533998612a028888ef76c4
MINJI <54696956+CoRoRo10@users.noreply.github.com>
2021-08-18 22:59:27 +0900

    Update choiminji_merge_sorted_array.js

--- 88.Merge_Sorted_Array/choiminji_merge_sorted_array.js
+++ 88.Merge_Sorted_Array/choiminji_merge_sorted_array.js
@@ -5,6 +5,20 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
- var merge = function(nums1, m, nums2, n) {
+const mergeNums = function(nums1, m, nums2, n) {
+  m--;
+  n--;
     
+  let index = nums1.length - 1;
+    
+  while (index >= 0) {
+    if (n > -1 && m > -1 && nums1[m] >= nums2[n]) {
+      nums1[index] = nums1[m];
+      nums1[m] = nums2[n];
+      m--;
+    } else if (n > -1) {
+      nums1[index] = nums2[n--];
+    }
+    index--;
+  }
 };

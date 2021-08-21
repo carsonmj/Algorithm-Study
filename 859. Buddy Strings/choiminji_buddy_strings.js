25980bd1d6a0956344ea9f4b35de5e515b64400b
mj <eunoia2040@gmail.com>
2021-08-21 14:25:41 +0900

    solved buddy strings

---
+++ 859. Buddy Strings/choiminji_buddy_strings.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+ var buddyStrings = function(s, goal) {
+    if (s.length != goal.length || s.length < 2 || goal.length < 2) {
+        return false;    
+    }
+    
+    if (s === goal)  {
+        const setStr = new Set(s.split(''));
+        return setStr.size < s.length;
+    }
+    
+    const difIndex = [];
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] != goal[i]) {
+            difIndex.push(i);
+        }
+        if (difIndex.length > 2) {
+            return false;    
+        }
+    }
+    
+    return s[difIndex[0]] === goal[difIndex[1]] && s[difIndex[1]] === goal[difIndex[0]];
+    
+};

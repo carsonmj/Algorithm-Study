aad13c2e67b6c1e1f83cd76f1d935c4798806b74
dndals <86826505+dndals@users.noreply.github.com>
2021-08-23 12:49:13 +0900

    Create Kiwoongmin_counting_Bits.js

---
+++ 338.Counting Bits/Kiwoongmin_counting_Bits.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var countBits = function(n) {
+    const arr = [];
+    let jinsoo = null;
+
+    for(let i = 0; i <= n; i++) {
+        jinsoo = i.toString(2);
+        arr[i] = Array.from(jinsoo, Number).reduce((acc, cur) => acc + cur);
+        
+    }
+   return arr;
+};

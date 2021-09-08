6b13be12347bde408eef3f64742d24901e319d98
Yang-woo <82132590+Yang-woo@users.noreply.github.com>
2021-09-08 14:54:57 +0900

    Create leeyangwoo_행렬의 덧셈.js

---
+++ 행렬의 덧셈/leeyangwoo_행렬의 덧셈.js
@@ -0,0 +1,12 @@
+function solution(arr1, arr2) {
+    var answer = [[]];
+
+    for (var i = 0; i < arr1.length; i++) {
+        answer[i] = new Array();
+        for (var j = 0; j < arr1[i].length; j++) {
+          answer[i][j] = arr1[i][j] + arr2[i][j];
+        }
+    }
+
+    return answer;
+}
